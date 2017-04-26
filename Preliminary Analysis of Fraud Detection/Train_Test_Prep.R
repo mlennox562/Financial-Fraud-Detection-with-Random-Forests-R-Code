06a7dc07cc76ab3f012330fba667eee147bc42b9
mlennox562 <mlennox562@hotmail.co.uk>
2017-04-26 10:36:34 +0100

    Code required to build the training and testing datasets.

--- Preliminary Analysis of Fraud Detection/Train_Test_Prep.R
+++ Preliminary Analysis of Fraud Detection/Train_Test_Prep.R
@@ -1,8 +1,7 @@
 # Firstly, the entire dataset is read in. 
 full <- read.csv('C:/Users/Mark/Documents/Credit Card Data/creditcard.csv')
 
-set.seed(927540672)
-
+set.seed(1234)
 
 full_random <- full[sample(nrow(full)),] # randomize row order
 train_fraction <- 0.7
@@ -12,5 +11,3 @@ identical(full_random, rbind(train_data, test_data)) # test that split was done 
 
 write.csv(train_data, 'C:/Users/Mark/Documents/Credit Card Data/train.csv')
 write.csv(test_data, 'C:/Users/Mark/Documents/Credit Card Data/test.csv')
-
-test_data_Class <- test_data[,32]
