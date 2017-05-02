8d20ad5dad630fd69f3a63baaed733c48f75ba63
mlennox562 <mlennox562@hotmail.co.uk>
2017-05-02 11:03:41 +0100

    Create balancedData_Over.R

---
+++ Techniques to Deal with Imbalance/balancedData_Over.R
@@ -0,0 +1,25 @@
+library(unbalanced)
+
+#read the training data into R
+
+train_data <- read.csv('/users/mlennox05/Documents/creditcard/train.csv')
+
+train_data <- train_data[,-1]
+
+table(train_data$Class)
+
+#run the Over-Sample algorithm within the unbalanced package.
+
+n<-ncol(train_data)
+output<-train_data$Class
+input<-train_data[ ,-n]
+data<-ubOver(X=input, Y= output)
+newData<-cbind(data$X, data$Y)
+
+table(newData$`data$Y`)
+
+colnames(newData) <- c("id","Time","V1","V2","V3","V4","V5","V6","V7","V8","V9","V10","V11","V12","V13","V14","V15","V16","V17","V18","V19","V20","V21","V22","V23","V24","V25","V26","V27","V28","Amount","Class")
+
+#write the new training dataset to a csv file 
+
+write.csv(newData, '/users/mlennox05/Documents/creditcard/balancedData_Over.csv')
