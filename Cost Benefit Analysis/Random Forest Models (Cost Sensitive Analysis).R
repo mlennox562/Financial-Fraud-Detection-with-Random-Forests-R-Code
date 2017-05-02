8355c32a2e36241e59a6721e0d26562382fde2d9
mlennox562 <mlennox562@hotmail.co.uk>
2017-05-02 11:27:38 +0100

    Update Random Forest Models (Cost Sensitive Analysis).R

--- Cost Benefit Analysis/Random Forest Models (Cost Sensitive Analysis).R
+++ Cost Benefit Analysis/Random Forest Models (Cost Sensitive Analysis).R
@@ -4,6 +4,8 @@ parallelStartSocket(3)
 
 '========================================================================================================='
 
+#read the testing data into R
+
 test_data <- read.csv('/users/mlennox05/Documents/creditcard/test.csv')
 
 test_data <- test_data[,-1]
@@ -16,16 +18,24 @@ test_data$Class[test_data$Class==1] <- 'Bad'
 
 test.task = makeClassifTask(data = test_data, target = "Class",positive = "Bad")
 
+#Let c_a denote the cost of analysis
+
 c_a <- 1
+
+#build a cost matrix 
 
 costs = matrix(c(c_a, c_a, 100*c_a, 0), 2)
 
 colnames(costs) = rownames(costs) = getTaskClassLevels(test.task)
 
+#In order to calculate the average costs over the entire data set we first need to create a new performance Measure using makeCostMeasure
+
 credit.costs = makeCostMeasure(id = "credit.costs", name = "Credit costs", costs = costs,
                                best = 0, worst = 100*c_a)
 
 '========================================================================================================='
+
+#read the under-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_Under.csv")
 
@@ -39,9 +49,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#threshold
+
 th = costs[2,1]/(costs[2,1] + costs[1,2] - costs[1,1] - costs[2,2])
 
 # lrn = makeLearner("classif.randomForest", predict.type = "prob")
+
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
 
 rin = makeResampleInstance("CV", iters = 3, task = train.task)
 lrn = makeLearner("classif.randomForest", predict.type = "prob", predict.threshold = th)
@@ -50,12 +64,16 @@ r = resample(lrn, train.task, resampling = rin, measures = list(credit.costs, mm
 # mod = train(lrn, train.task)
 # pred = predict(mod, task = test.task)
 
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
+
 cat("Under:") 
 performance(setThreshold(r$pred, 0.5), measures = list(credit.costs, mmce))
 
 rm(train_data)
 
 '========================================================================================================='
+
+#read the SMOTE-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_SMOTE.csv")
 
@@ -69,9 +87,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("SMOTE:") 
 performance(pred, measures = list(credit.costs, mmce))
@@ -79,6 +101,8 @@ performance(pred, measures = list(credit.costs, mmce))
 rm(train_data)
 
 '========================================================================================================='
+
+#read the CNN-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_CNN.csv")
 
@@ -92,9 +116,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("CNN:") 
 performance(pred, measures = list(credit.costs, mmce))
@@ -102,6 +130,8 @@ performance(pred, measures = list(credit.costs, mmce))
 rm(train_data)
 
 '========================================================================================================='
+
+#read the ENN-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_ENN.csv")
 
@@ -115,9 +145,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("ENN:") 
 performance(pred, measures = list(credit.costs, mmce))
@@ -125,6 +159,8 @@ performance(pred, measures = list(credit.costs, mmce))
 rm(train_data)
 
 '========================================================================================================='
+
+#read the Tomek_link-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_Tomek.csv")
 
@@ -138,9 +174,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("Tomek:") 
 performance(pred, measures = list(credit.costs, mmce))
@@ -148,6 +188,8 @@ performance(pred, measures = list(credit.costs, mmce))
 rm(train_data)
 
 '========================================================================================================='
+
+#read the OSS-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_OSS.csv")
 
@@ -161,9 +203,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("OSS:") 
 performance(pred, measures = list(credit.costs, mmce))
@@ -171,6 +217,8 @@ performance(pred, measures = list(credit.costs, mmce))
 rm(train_data)
 
 '========================================================================================================='
+
+#read the NCL-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_NCL.csv")
 
@@ -184,9 +232,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("NCL:") 
 performance(pred, measures = list(credit.costs, mmce))
@@ -194,6 +246,8 @@ performance(pred, measures = list(credit.costs, mmce))
 rm(train_data)
 
 '========================================================================================================='
+
+#read the training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/train.csv")
 
@@ -207,9 +261,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("No Re-Balancing:") 
 performance(pred, measures = list(credit.costs, mmce))
@@ -217,6 +275,8 @@ performance(pred, measures = list(credit.costs, mmce))
 rm(train_data)
 
 '========================================================================================================='
+
+#read the Over-sampled training dataset into R
 
 train_data <- read.csv("/users/mlennox05/Documents/creditcard/balancedData_Over.csv")
 
@@ -230,9 +290,13 @@ train_data$Class[train_data$Class == 0] <- 'Good'
 
 train.task = makeClassifTask(data = train_data, target = "Class",positive = "Bad")
 
+#In the R code below we make use of the predict.threshold argument of makeLearner to set the threshold before doing a 3-fold cross-validation on the credit.task. 
+
 lrn = makeLearner("classif.randomForest", predict.type = "prob")
 mod = train(lrn, train.task)
 pred = predict(mod, task = test.task)
+
+#Then the average costs can be computed by function performance. Below we compare the average costs and the error rate (mmce)
 
 cat("Over:") 
 performance(pred, measures = list(credit.costs, mmce))
